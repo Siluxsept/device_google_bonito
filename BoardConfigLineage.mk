LineageOS/android_device_google_bonito

b4s4: Include device FCM from vendor/lineage
Sadly we aren't able to include it globally without breaking other
devices ;((((

Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723

BoardConfigLineage.mk
@@ -30,7 +30,7 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/bonito/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/bonito/lineage_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \

Siluxsept/device_google_bonito

BoardConfigLineage.mk
@@ -33,7 +33,7 @@ TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/bonito/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += device/google/bonito/lineage_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aicp/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \